a16e29d488711817ef7be626fdee7ee1f0634259
Juan M. Panasiti <juanpanasiti@gmail.com>
2017-02-11 08:35:52 -0300

    Configurado lo mínimo de material design usando el template 'Scrollable tabs'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,29 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require material
+//= require material/mdlComponentHandler
+//= require material/rAF
+//= require material/button
+//= require material/checkbox
+//= require material/data-table
+//= require material/icon-toggle
+//= require material/layout
+//= require material/menu
+//= require material/progress
+//= require material/radio
+//= require material/ripple
+//= require material/slider
+//= require material/snackbar
+//= require material/spinner
+//= require material/switch
+//= require material/tabs
+//= require material/textfield
+//= require material/tooltip
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+document.addEventListener('turbolinks:load', function() {
+  componentHandler.upgradeDom();
+});
