edaf0c92404af94685e5ebc3e9ec6ace22863453
Juan M. Panasiti <juanpanasiti@gmail.com>
2018-01-24 14:24:23 -0300

    Modificaciones en la vista de control_csv y helper para el estado de los imei (si coinciden en sistema)

--- app/helpers/lines_helper.rb
+++ app/helpers/lines_helper.rb
@@ -1,7 +1,7 @@
 module LinesHelper
-  def get_class_to_registered_imei(reg_imei,act_imei)
+  def get_class_to_registered_imei(reg_imei,act_imei,sys_imei)
     css_class = ''
-    if reg_imei == act_imei
+    if reg_imei == act_imei && act_imei == sys_imei && reg_imei == sys_imei
       css_class = 'table-success'
     else
       css_class = 'table-warning'
